2c017454a8eff9aebb42646499682970af41fb21
Devinder Sharma <dev@Devs-MacBook-Air.local>
2025-04-01 03:14:25 +0530

    .

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:5175', 'http://localhost:5176'],
+  origin: ['https://pizza-frontend-jet.vercel.app/', 'http://localhost:5174', 'http://localhost:5175', 'http://localhost:5176'],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true,
   allowedHeaders: ['Content-Type', 'Authorization']
